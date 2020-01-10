169ca868caa82cc7d00c5649effd42cebba2ab1b
Erik Sillén <erik.sillen.1990@gmail.com>
2020-01-10 17:12:40 +0100

    bugfix

--- label_writer.py
+++ label_writer.py
@@ -3,8 +3,8 @@ import sys
 from mfrc522 import SimpleMFRC522
 nfc = SimpleMFRC522()
 
-def write(filename):
-    nfc.write(filename)
+def write(data):
+    nfc.write(data)
 
 if __name__ == "__main__":
     try:
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             user_input = input("Text to print to card: ")
             print("Hold a tag near the writer")
             write(user_input)
-            print("wrote %s to card!" % (filename))
+            print("wrote %s to card!" % (user_input))
             sleep(5)
     except KeyboardInterrupt:
         GPIO.cleanup()
